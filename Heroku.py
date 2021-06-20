41d4fd2c7c13425c4c20e9869efbd940cb16e8f4
chetansoma <scksri@gmail.com>
2021-06-20 06:21:55 +0100

    This second commit

--- Heroku.py
+++ Heroku.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['GET','POST'])
 def index():
-    return "<h1> This is flask application <h1>"
+    return "<h1> This is flask application New update <h1>"
 
 if __name__ == "__main__":
     app.run()
